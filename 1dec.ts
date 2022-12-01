1772c6e68721bbc1c70abf527e83982b7c207cd8
Adin Ehnsiö <adinehnsio@gmail.com>
2022-12-01 16:35:21 +0100

    1dec

--- 1dec.ts
+++ 1dec.ts
@@ -1,23 +1,22 @@
 // npx ts-node 1dec.ts
 const fs = require("fs");
-const data: string = fs.readFileSync("./data/1dec.txt", "utf8");
 // const data: string = fs.readFileSync("./samples/1dec.txt", "utf8");
-const foods = data
-  .split("\n\n")
-  .map((f: String) => f.split("\n").map((e) => Number(e)));
+const data: string = fs.readFileSync("./data/1dec.txt", "utf8");
 
 console.time("part 1");
-const calories = foods.map((f: Array<number>) =>
-  f.reduce((a: number, b: number) => a + b, 0)
-);
-const max = Math.max(...calories);
-console.timeEnd("part 1");
+const numbers = data
+  .split("\n\n")
+  .map((l) => l.split("\n").reduce((a, b) => Number(a) + Number(b), 0));
+
+const max = Math.max(...numbers);
 console.log(max);
+console.timeEnd("part 1");
 
 console.time("part 2");
-const total = calories
-  .sort((a: number, b: number) => b - a)
+const topThree = numbers
+  .sort((a, b) => b - a)
   .slice(0, 3)
-  .reduce((a: number, b: number) => a + b, 0);
+  .reduce((a, b) => a + b);
+console.log(topThree);
+
 console.timeEnd("part 2");
-console.log(total);
